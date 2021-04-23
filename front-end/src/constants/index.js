ca9df6401a8def9e84ba68e37514415138d9b110
carlos pupo <carlos.pupo@jobsity.io>
2021-04-23 15:03:38 -0500

    docker support.

--- front-end/src/constants/index.js
+++ front-end/src/constants/index.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://localhost:8080/jakarta-app/rest/v1';
-export const API_NOTIFICATION_URL = 'http://localhost:9090/v1/emails';
+export const API_BASE_URL = process.env.API_BASE_URL ? process.env.API_BASE_URL : 'http://localhost:8080/jakarta-app/rest/v1';
+export const API_NOTIFICATION_URL = process.env.API_NOTIFICATION_URL ? process.env.API_NOTIFICATION_URL : 'http://localhost:9090/v1/emails';
--- jakarta-app/src/main/java/com/stream/KafkaProducer.java
+++ jakarta-app/src/main/java/com/stream/KafkaProducer.java
@@ -8,14 +8,22 @@ import java.util.Properties;
 
 public class KafkaProducer {
 
+    private static String getFromEnv(String key, String defaultValue) {
+        String result = System.getenv(key);
+        if(result == null || result.trim().isEmpty()) {
+            return defaultValue;
+        }
+        return result;
+    }
+
     public static void sendMessage(String key, String message) {
         final Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, getFromEnv("KAFKA_URL", "localhost:9092"));
         props.put(ProducerConfig.ACKS_CONFIG, "all");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
-        final String topic = "notifications";
+        final String topic = getFromEnv("KAFKA_TOPIC", "notifications");
 
         Producer<String, String> producer = new org.apache.kafka.clients.producer.KafkaProducer<>(props);
 
